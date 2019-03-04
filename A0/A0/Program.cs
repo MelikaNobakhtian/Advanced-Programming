c7c19e09703b2e72305aa554066ade6dcee54b37
melikano200 <haminob48@gmail.com>
2019-03-04 13:30:06 +0330

    Add

---
+++ A0/A0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A0
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double Sqrt(long a)
+        {
+            return Math.Sqrt(a);
+        }
+        public static long Square(long a)
+        {
+            return a * a;
+        }
+        public static long Product(long a, long b)
+
+        {
+            return a * b;
+
+        }
+        public static long Add(long a,long b)
+        {
+            return a + b;
+        }
+        public static long Negate(long a)
+        {
+            return -a;
+        }
+        public static long Subtract(long a,long b)
+        {
+            return a - b;
+        }
+        public static long Factoriel( long a)
+        {
+            long factoriel = 1;
+            for (long i = a; i > 0; i--)
+                factoriel *= i;
+            return factoriel;
+        }
+    }
+}
---
+++ A0/A0Tests/ProgramTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A0.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void SqrtTest()
+        {
+            double expectedresult = 2;
+            double functionresult = Program.Sqrt(4);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void SquareTest()
+        {
+            long expectedresult = 25;
+            long functionresult = Program.Square(5);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void ProductTest()
+        {
+            long expectedresult = 6;
+            long functionresult = Program.Product(2, 3);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            long expectedresult = 4;
+            long functionresult = Program.Add(1, 3);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void NegateTest()
+        {
+            long expectedresult = 4;
+            long functionresult = Program.Negate(-4);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            long expectedresult = 4;
+            long functionresult = Program.Subtract(7, 3);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void FactorielTest()
+        {
+            long expectedresult = 6;
+            long functionresult = Program.Factoriel( 3);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+    }
+}
