10cb6a61468b84387bb8e95aaf7cd39686bc05ab
melikano200 <haminob48@gmail.com>
2019-03-06 16:09:19 +0330

    methods and tests almost are completed

---
+++ A1S1/A1S1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3
+    {
+    public class Program
+        {
+        public static void Main(string[] args)
+            {
+            }
+        public int CaculateLength(string str)
+            {
+            // Write your code here and remove next line
+            return str.Length;
+            }
+        
+        public int LetterCount(string str)
+            {
+            // Write your code here and remove next line
+            int num = 0;
+            foreach(char letter in str)
+            {
+                if (Char.IsLetter(letter))
+                    num++;
+
+            }
+            return num;
+            }
+        
+        public int LineCount(string str)
+            {
+            // Write your code here and remove next line
+            string[] lines = str.Split('\n');
+            return lines.Length;
+            }
+        
+        public static int FileLineCount(string filePath)
+            {
+            
+            string[] lines = File.ReadAllLines(filePath);
+            return lines.Length;
+            }
+        
+        public static string[] ListFiles(string dirPath)
+            {
+            // Write your code here and remove next line
+            
+            return Directory.GetFiles(dirPath);
+            }
+        
+        public static double FileSize(string filePath)
+            {
+            int charcount = 0;
+            string[] lines = File.ReadAllLines(filePath);
+            int num = FileLineCount(filePath);
+            for (int i = 0; i < num; i++)
+                charcount += lines[i].Length;
+            return charcount;
+
+            
+            }
+        }
+    }
---
+++ A1S1/A1S1Tests/ProgramTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace A3.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        private static string GetTestFile(out int lineCount, out int charCount)
+        {
+            charCount = 0;
+            string tmpFile = Path.GetTempFileName();
+            lineCount = new Random(0).Next(10, 100);
+            List<string> lines = new List<string>();
+            for (int i = 0; i < lineCount; i++)
+            {
+                string line = $"Line number {i}";
+                charCount += line.Length;
+                lines.Add(line);
+            }
+            File.WriteAllLines(tmpFile, lines);
+            return tmpFile;
+        }
+
+        private static string[] GetTestDir(out string tmpDir)
+
+        {
+        tmpDir = Path.GetTempFileName();
+        Directory.CreateDirectory(tmpDir);
+         int rndNum = new Random(0).Next(10, 20);
+        List<string> files = new List<string>();
+        for (int i=0; i<rndNum; i++)
+         {
+            string fileName = Path.Combine(tmpDir, $"file{i}.txt");
+            File.WriteAllText(fileName, $"file{i}.txt content");
+            files.Add(fileName);
+         }
+         return files.ToArray();
+ }
+
+[TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CaculateLengthTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LetterCountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LineCountTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void FileLineCountTest()
+        {
+            int lineCount;
+            int charCount;
+            string path = GetTestFile(out lineCount, out charCount);
+            int functionresult = Program.FileLineCount(path);
+            Assert.AreEqual(lineCount, functionresult);
+
+
+        }
+
+        [TestMethod()]
+        public void ListFilesTest()
+        {
+            string tmpdir;
+            string[] expectedresult = GetTestDir(out tmpdir);
+            string[] functionresult = Program.ListFiles(tmpdir);
+            Assert.AreEqual(expectedresult, functionresult);
+        }
+
+        [TestMethod()]
+        public void FileSizeTest()
+        {
+            int lineCount;
+            int charCount;
+            string path = GetTestFile(out lineCount, out charCount);
+            double functionresult = Program.FileSize(path);
+            Assert.AreEqual(charCount, functionresult);
+
+
+        }
+    }
+}
