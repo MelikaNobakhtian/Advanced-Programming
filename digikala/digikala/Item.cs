05c10eb3a1e703dbe01a0623ed7d772aa870f87b
melikano200 <haminob48@gmail.com>
2019-05-13 17:00:25 +0430

    LAB 2

---
+++ digikala/digikala/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace digikala
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price;
+        public DateTime RealeaseDate { get; set; }
+        public string Brand { get; set; }
+        public string Seller { get; set; }
+
+        public Item(string title,double price,DateTime realeasedate,string brand,string seller)
+        {
+            
+            Title = title;
+            Price = price;
+            RealeaseDate = realeasedate;
+            Brand = brand;
+            Seller = seller;
+            
+        }
+
+        public double UpdatePrice(double newprice)
+        {
+            if (newprice <= (1.2) * Price )
+                return newprice;
+                return (1.2) * Price;
+
+        }
+            
+
+    }
+}
