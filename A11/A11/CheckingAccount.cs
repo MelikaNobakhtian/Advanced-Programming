25c76ce63056b9e622312f4cb32493917bf18547
melikano200 <haminob48@gmail.com>
2019-06-02 22:17:21 +0430

    correcting transaction

--- A11/A11/CheckingAccount.cs
+++ A11/A11/CheckingAccount.cs
@@ -4,16 +4,16 @@ namespace A11
 {
     public class CheckingAccount : Account
     {
-        public double TransActionFee { get; set; }
+        public double TransactionFee { get; set; }
 
         public CheckingAccount(int balance, int transactionFee) : base(balance)
         {
-            this.TransActionFee = transactionFee;
+            this.TransactionFee = transactionFee;
         }
 
-        public override void Credit(double amount) => base.Credit(amount - TransActionFee);
+        public override void Credit(double amount) => base.Credit(amount - TransactionFee);
 
-        public override bool Debit(double amount) => base.Debit(amount + TransActionFee);
+        public override bool Debit(double amount) => base.Debit(amount + TransactionFee);
 
     }
 }
